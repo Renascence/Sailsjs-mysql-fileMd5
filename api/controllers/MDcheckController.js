e7af811ccebf3d96118ab5f35c7f2e9d56694623
Renascence <863692900@qq.com>
2016-04-26 14:38:55 +0800

    success add mysql in app

--- api/controllers/MDcheckController.js
+++ api/controllers/MDcheckController.js
@@ -6,19 +6,25 @@
  */
 
 module.exports = {
-  hi: function (req, res) {
+  hi: function(req, res) {
     var json = {
-      "tc":'1',
-      "ali":'2',
-      'baidu':'3'
+      "tc": '1',
+      "ali": '2',
+      'baidu': '3'
     }
     return res.send(json);
   },
-  get: function (req,res) {
-    return res.getMd5.findOne({id:'1'})
-  },
-  bye: function (req, res) {
-    return res.redirect("http://www.sayonara.com");
+  get: function(req, res) {
+    getMd5.findOne({id:"1"})
+      .exec(function(err, md5) {
+        if (err) {
+          console.log('err',err)
+          return res.json(err)
+        }
+        console.log("md5",md5)
+        return res.send(md5);
+      })
+    // console.log(data)
+    // return res.send(data.data)
   }
 };
-
--- api/models/getMd5.js
+++ api/models/getMd5.js
@@ -5,37 +5,37 @@ module.exports = {
   autoUpdatedAt: false,
   attributes: {
     id: {
-      type: 'int',
+      type: 'string',
       unique: true,
       primaryKey: true,
-    },
-    hot: {
-      type: 'int'
     },
     md5: {
       type: 'string'
     },
+    hot: {
+      type: 'string'
+    },
     name: {
       type: 'string'
+    },
+    toJSON: function() {
+      var obj = this.toObject();
+      delete obj.id;
+      delete obj.hot;
+      delete obj.md5;
+      delete obj.name;
+      // delete obj.attivo;
+      // delete obj.data_inserimento;
+      // delete obj.data_modifica;
+      // delete obj.idadm_amministratore_inserimento;
+      // delete obj.idadm_amministratore_modifica;
+      // delete obj.ip_addr;
+      // delete obj.pie_pagina;
+      // delete obj.parole_chiave_ricerca;
+      // delete obj.in_newsletter;
+      // delete obj.in_evidenza;
+      // delete obj.articolo_riservato;
+      // return obj;
     }
-    // toJSON: function () {
-    //     var obj = this.toObject();
-    //     delete obj.id_autore;
-    //     delete obj.id_news_categ;
-    //     delete obj.date;
-    //     delete obj.data_scadenza;
-    //     delete obj.attivo;
-    //     delete obj.data_inserimento;
-    //     delete obj.data_modifica;
-    //     delete obj.idadm_amministratore_inserimento;
-    //     delete obj.idadm_amministratore_modifica;
-    //     delete obj.ip_addr;
-    //     delete obj.pie_pagina;
-    //     delete obj.parole_chiave_ricerca;
-    //     delete obj.in_newsletter;
-    //     delete obj.in_evidenza;
-    //     delete obj.articolo_riservato;
-    //     return obj;
-    // }
   }
 };
